8e8e6a4eddc0640189e4ba102b06d2f6c0e0c7c7
Sifat <sifatur.rahman.sifat@dohatec.com.bd>
2025-09-05 13:53:10 +0600

    first user commit

---
+++ src/main/java/com/example/ShopSphere_eureka_service/ShopSphereEurekaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ShopSphere_eureka_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopSphereEurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopSphereEurekaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ShopSphere_eureka_service/ShopSphereEurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShopSphere_eureka_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopSphereEurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
